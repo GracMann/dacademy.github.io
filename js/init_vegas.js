646514a2c5dc58e21adab963e11addb66d1d9eca
Manoj Meena <gracious.mann@gmail.com>
2023-01-26 21:30:21 +0530

    deleted

--- js/init_vegas.js
+++
@@ -1,9 +0,0 @@
-$.vegas('slideshow', {
-  backgrounds:[
-  { src:'images/bg/8.jpg', fade:2000 },
-  { src:'images/bg/7.jpg', fade:2000 },
-  { src:'images/bg/6.jpg', fade:2000 },
-
-  ]
-});
-$('#vegas-next').click(function(){$.vegas('next');return false;});$('#vegas-prev').click(function(){$.vegas('previous');return false;});
--- js/smoothscroll.js
+++
@@ -1,7 +0,0 @@
-// SmoothScroll v0.9.9
-// Licensed under the terms of the MIT license.
-
-// People involved
-// - Balazs Galambosi: maintainer (CHANGELOG.txt)
-// - Patrick Brunner (patrickb1991@gmail.com)
-// - Michael Herf: ssc_pulse Algorithm
